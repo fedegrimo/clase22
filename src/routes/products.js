70a950480bc96ed13a0836575842a695e9b0c2d6
fede.grimo <fede.grimo@gmail.com>
2021-11-16 05:25:44 -0300

    [ADD] Multer Upload Image

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,6 +1,20 @@
 // ************ Require's ************
 const express = require('express');
 const router = express.Router();
+const multer = require("multer");
+const { path } = require('../app');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images/uploads')
+  },
+  filename: function (req, file, cb) {
+    const newFile = file.fieldname + '-' + Date.now();
+    cb(null, newFile)
+  }
+})
+
+const upload = multer({ storage: storage })
+//router.post('/register', upload.any(),usersController.save);
 
 // ************ Controller Require ************
 const productsController = require('../controllers/productsController');
@@ -10,7 +24,7 @@ router.get('/', productsController.index);
 
 /*** CREATE ONE PRODUCT ***/ 
 router.get('/create/', productsController.create); 
-router.post('/', productsController.store); 
+router.post('/', upload.single('fileImage'),productsController.store); 
 
 
 /*** GET ONE PRODUCT ***/ 
@@ -18,7 +32,7 @@ router.get('/detail/:id/', productsController.detail);
 
 /*** EDIT ONE PRODUCT ***/ 
 router.get('/:id/edit', productsController.edit); 
-router.put('/:id', productsController.update); 
+router.put('/:id', upload.single('fileImage'),productsController.update); 
 
 
 /*** DELETE ONE PRODUCT***/ 
